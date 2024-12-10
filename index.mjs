b79ac64d47267838207333782edefd2b6a8f3748
ktagdiwala <krishna.tagdiwala@gmail.com>
2024-12-10 02:42:10 -0800

    Functionality to update/delete custom set

--- index.mjs
+++ index.mjs
@@ -53,15 +53,45 @@ app.post("/customSets/new", async function(req, res){
              {"message": "Custom set added!"});
 });// Add customized set to user collection
 
-// app.get("/customSets/edit", async function(req, res){
-//   let set_id = req.query.set_id;
-//   let sql = `SELECT *, 
-//               FROM custom_set
-//               WHERE set_id =  ?`;
-//   let params = [set_id];
-//   const [rows] = await conn.query(sql, params);
-//   res.render("editCustomSet", {"set":rows});
-// });// edit existing set
+app.get("/customSets/edit", async function(req, res){
+  let set_id = req.query.setId;
+  console.log("Set id = " + set_id);
+  let sql = `SELECT * 
+              FROM custom_set
+              WHERE set_id =  ?`;
+  let params = [set_id];
+  const [rows] = await conn.query(sql, params);
+  res.render("editCustomSet", {"set":rows});
+});// edit existing set
+
+app.post("/customSets/edit", async function(req, res){
+  let set_id = req.body.set_id;
+  let type = req.body.q1;
+  let size = req.body.q2;
+  let length = req.body.q3;
+  let description = req.body.q4;
+  let name = req.body.q5;
+  let sql = `UPDATE custom_set
+            SET name = ?,
+            type = ?,
+            size = ?,
+            length = ?,
+            description = ?
+            WHERE set_id = ?`;
+  let params = [name, type, size, length, description, set_id];  
+  const [rows] = await conn.query(sql,params);
+  res.redirect("/customSets");
+});// update set info (based on edits) in database
+
+app.get("/customSets/delete", async function(req, res){
+  let set_id = req.query.setId;
+  let sql = `DELETE
+              FROM custom_set
+              WHERE set_id = ?`;
+  const [rows] = await conn.query(sql, [set_id]);
+
+  res.redirect("/customSets");
+});// delete custom set
 
 app.get("/dbTest", async(req, res) => {
     let sql = `SELECT *
