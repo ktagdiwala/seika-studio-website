7669a788860c42209b3887799540c1ad833072b6
Jesus9498 <Jesusmartinez9498@gmail.com>
2024-12-10 13:09:21 -0800

    created new brnach (broke the other one) implemented bookAnAppointment.ejs, added to styles.css to center google form, and added route on index.mjs.

--- index.mjs
+++ index.mjs
@@ -37,6 +37,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/bookAnAppointment", (req, res) => {
+  res.render("bookAnAppointment");
+});
+
 app.get("/customSets", async (req, res)=>{
   let sql = `SELECT *
             FROM custom_set
