53ada7f3b6c6e48e0a5f3c321ccece65a3cca455
Jorge Vazquez <jav200021@gmail.com>
2024-12-11 15:42:35 -0800

    Putting back what Walid had done for the Product Gallery that I had removed when merging Krishna's PR.

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,21 @@
 
-
-
+// Event listeners
+let productLinks = document.querySelectorAll("a.gallery");
+for (let productLink of productLinks) {
+    productLink.addEventListener("click", getArtInfo);
+}
+async function getArtInfo() {
+    var myModal = new bootstrap.Modal(document.getElementById('galleryModal'));
+    myModal.show();
+    let url = `/api/product/${this.id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let productInfo = document.querySelector("#productInfo");
+    productInfo.innerHTML = `
+        <div>
+            <strong>${data[0].title}</strong><br>
+            <p>${data[0].description}</p>
+            <p><strong>Price:</strong> $${data[0].price}</p>
+            <p><strong>Category:</strong> ${data[0].category}</p>
+        </div>`;
+}
