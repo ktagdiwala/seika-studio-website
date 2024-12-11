669cb584ebac00496eaffef7499780c5b17270d3
jorgevaz47 <jav200021@gmail.com>
2024-12-11 11:44:24 -0800

    Moved routes around and added comments to section off and organize code for readability. Adjusted the two auth functions and updated their usage appropiately to each route

--- index.mjs
+++ index.mjs
@@ -5,6 +5,10 @@ import session from "express-session";
 
 const app = express();
 
+// =====================
+// Express Session Setup
+// =====================
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -27,6 +31,10 @@ app.use((req, res, next) => {
   next();
 });
 
+// =========================
+// Database Connection Setup
+// =========================
+
 //setting up database connection pool
 const pool = mysql.createPool({
   host: "walid-elgammal.online",
@@ -38,139 +46,30 @@ const pool = mysql.createPool({
 });
 const conn = await pool.getConnection();
 
-//routes
+// ==========
+//   Routes
+// ==========
+
+// --- Home Page ---
 app.get("/", (req, res) => {
   console.log("Rendering to /index");
   res.render("index");
 });
 
+// --- Booking Page ---
 app.get("/bookAnAppointment", (req, res) => {
   console.log("Rendering to /bookAnAppointment");
   res.render("bookAnAppointment");
 });
 
-app.get("/signUp", isNotAuthenticated("/profile"), (req, res) => {
-  console.log("Rendering to /signUp");
-  res.render("signUp");
-});
-
-app.get("/productGallery", async (req, res) => {
-  let sql = `SELECT *
-          FROM product
-          ORDER BY category`;
-  const [rows] = await conn.query(sql);
-  console.log("Rendering to /productGallery");
-  res.render("productGallery", { picGallery: rows });
-});
-
-// Web API
-app.get("/api/product/:id", async (req, res) => {
-  let product_id = req.params.id;
-  let sql = `SELECT *
-             FROM product
-             WHERE product_id = ?`;
-  let [rows] = await conn.query(sql, [product_id]);
-  res.send(rows);
-});
-
-app.get("/customSets", isAuthenticated("/login"), async (req, res) => {
-  // let user_id = SESSION USER ID
-  let sql = `SELECT *
-    FROM custom_set
-    ORDER BY name`;
-  // let params = [user_id];
-  const [rows] = await conn.query(sql);
-  console.log("Rendering to /customSets");
-  res.render("customSets", { sets: rows });
-}); // View custom nail sets created by user
-
-app.get("/customSets/new", isAuthenticated("/login"), (req, res) => {
-  console.log("Rendering to /createCustomSet");
-  res.render("createCustomSet");
-}); // Create a custom nail set
-
-app.post(
-  "/customSets/new",
-  isAuthenticated("/login"),
-  async function (req, res) {
-    let user_id = req.body.user_id;
-    let type = req.body.q1;
-    let size = req.body.q2;
-    let length = req.body.q3;
-    let description = req.body.q4;
-    let name = req.body.q5;
-    let sql = `INSERT INTO custom_set
-             (user_id, name, type, size, length, description)
-              VALUES (?, ?, ?, ?, ?, ?)`;
-    let params = [user_id, name, type, size, length, description];
-    const [rows] = await conn.query(sql, params);
-    console.log("Rendering to /createCustomSet");
-    res.render("createCustomSet", { message: "Custom set added!" });
-  }
-); // Add customized set to user collection
-
-app.get(
-  "/customSets/edit",
-  isAuthenticated("/login"),
-  async function (req, res) {
-    let set_id = req.query.setId;
-    console.log("Set id = " + set_id);
-    let sql = `SELECT * 
-              FROM custom_set
-              WHERE set_id =  ?`;
-    let params = [set_id];
-    const [rows] = await conn.query(sql, params);
-    console.log("Rendering to /editCustomSet");
-    res.render("editCustomSet", { set: rows });
-  }
-); // edit existing set
-
-app.post(
-  "/customSets/edit",
-  isAuthenticated("/login"),
-  async function (req, res) {
-    let set_id = req.body.set_id;
-    let type = req.body.q1;
-    let size = req.body.q2;
-    let length = req.body.q3;
-    let description = req.body.q4;
-    let name = req.body.q5;
-    let sql = `UPDATE custom_set
-            SET name = ?,
-            type = ?,
-            size = ?,
-            length = ?,
-            description = ?
-            WHERE set_id = ?`;
-    let params = [name, type, size, length, description, set_id];
-    const [rows] = await conn.query(sql, params);
-    console.log("Redirecting to /customSets");
-    res.redirect("/customSets");
-  }
-); // update set info (based on edits) in database
-
-app.get("/customSets/delete", isAuthenticated, async function (req, res) {
-  let set_id = req.query.setId;
-  let sql = `DELETE
-              FROM custom_set
-              WHERE set_id = ?`;
-  const [rows] = await conn.query(sql, [set_id]);
-
-  console.log("Redirecting to /customSets");
-  res.redirect("/customSets");
-}); // delete custom set
-
-app.get("/profile", isNotAuthenticated("/login"), (req, res) => {
-  console.log("Rendering to /profile");
-  res.render("/profile");
-});
-
+// --- Login Page ---
+// GET Route
 app.get("/login", isAuthenticated("/profile"), (req, res) => {
   console.log("Rendering to /login");
   res.render("login");
 });
 
-// TODO: Seprate Login and Signup functionality -> app.post for /login and /signUp as signUp requires more fields
+// POST Route
 app.post("/login", async (req, res) => {
   let username = req.body.email;
   let password = req.body.password;
@@ -197,13 +96,21 @@ app.post("/login", async (req, res) => {
     req.session.authenticated = true;
     req.session.userId = rows[0].user_id;
     console.log("Rendering to /index");
-    res.render("index");
+    res.render("profile");
   } else {
     console.log("Rendering to /login");
     res.render("login", { message: "Incorrect username or password." });
   }
 });
 
+// --- Sign Up Page ---
+// GET Route
+app.get("/signUp", isAuthenticated("/profile"), (req, res) => {
+  console.log("Rendering to /signUp");
+  res.render("signUp");
+});
+
+// POST Route
 // app.post("/signUp", async (req, res) => {
 //   let username = req.body.email;
 //   let password = req.body.password;
@@ -238,7 +145,14 @@ app.post("/login", async (req, res) => {
 //   }
 // });
 
-// Simple logout route
+// --- Profile Page ---
+app.get("/profile", isNotAuthenticated("/login"), (req, res) => {
+  console.log("Rendering to /profile");
+  console.log(!req.session.authenticated);
+  res.render("profile");
+});
+
+// --- Logout Route ---
 app.get("/logout", (req, res) => {
   req.session.destroy((err) => {
     if (err) {
@@ -250,28 +164,136 @@ app.get("/logout", (req, res) => {
   });
 });
 
-// "Langing Page" which is meant to be a Profile page. Thinking about changing this to "/profile" to make it more clear.
-// This route only works if you are signed in
-app.get("/profile", isAuthenticated("/login"), (req, res) => {
-  console.log("Rendering to /profile");
-  res.render("profile");
+// --- Product Gallery Page ---
+app.get("/productGallery", async (req, res) => {
+  let sql = `SELECT *
+          FROM product
+          ORDER BY category`;
+  const [rows] = await conn.query(sql);
+  console.log("Rendering to /productGallery");
+  res.render("productGallery", { picGallery: rows });
 });
 
-app.get("/dbTest", async (req, res) => {
+// Web API for Product Gallery
+app.get("/api/product/:id", async (req, res) => {
+  let product_id = req.params.id;
   let sql = `SELECT *
-                FROM product`;
-  const [rows] = await conn.query(sql);
+             FROM product
+             WHERE product_id = ?`;
+  let [rows] = await conn.query(sql, [product_id]);
   res.send(rows);
-}); //dbTest
+});
+
+// --- Custom Sets Page ---
+// TODO - Update this route to use the current user's ID
+app.get("/customSets", isNotAuthenticated("/login"), async (req, res) => {
+  // let user_id = SESSION USER ID
+  let sql = `SELECT *
+    FROM custom_set
+    ORDER BY name`;
+  // let params = [user_id];
+  const [rows] = await conn.query(sql);
+  console.log("Rendering to /customSets");
+  res.render("customSets", { sets: rows });
+}); // View custom nail sets created by user
+
+// --- New Custom Set Page ---
+// GET Route
+app.get("/customSets/new", isNotAuthenticated("/login"), (req, res) => {
+  console.log("Rendering to /createCustomSet");
+  res.render("createCustomSet");
+}); // Create a custom nail set
+
+// POST Route
+app.post(
+  "/customSets/new",
+  isNotAuthenticated("/login"),
+  async function (req, res) {
+    let user_id = req.body.user_id;
+    let type = req.body.q1;
+    let size = req.body.q2;
+    let length = req.body.q3;
+    let description = req.body.q4;
+    let name = req.body.q5;
+    let sql = `INSERT INTO custom_set
+             (user_id, name, type, size, length, description)
+              VALUES (?, ?, ?, ?, ?, ?)`;
+    let params = [user_id, name, type, size, length, description];
+    const [rows] = await conn.query(sql, params);
+    console.log("Rendering to /createCustomSet");
+    res.render("createCustomSet", { message: "Custom set added!" });
+  }
+); // Add customized set to user collection
+
+// --- Edit Custom Set Page ---
+// GET Route
+app.get(
+  "/customSets/edit",
+  isNotAuthenticated("/login"),
+  async function (req, res) {
+    let set_id = req.query.setId;
+    console.log("Set id = " + set_id);
+    let sql = `SELECT * 
+              FROM custom_set
+              WHERE set_id =  ?`;
+    let params = [set_id];
+    const [rows] = await conn.query(sql, params);
+    console.log("Rendering to /editCustomSet");
+    res.render("editCustomSet", { set: rows });
+  }
+); // edit existing set
+
+// POST Route
+app.post(
+  "/customSets/edit",
+  isNotAuthenticated("/login"),
+  async function (req, res) {
+    let set_id = req.body.set_id;
+    let type = req.body.q1;
+    let size = req.body.q2;
+    let length = req.body.q3;
+    let description = req.body.q4;
+    let name = req.body.q5;
+    let sql = `UPDATE custom_set
+            SET name = ?,
+            type = ?,
+            size = ?,
+            length = ?,
+            description = ?
+            WHERE set_id = ?`;
+    let params = [name, type, size, length, description, set_id];
+    const [rows] = await conn.query(sql, params);
+    console.log("Redirecting to /customSets");
+    res.redirect("/customSets");
+  }
+); // update set info (based on edits) in database
+
+// --- Delete Custom Set Page ---
+app.get(
+  "/customSets/delete",
+  isNotAuthenticated("/login"),
+  async function (req, res) {
+    let set_id = req.query.setId;
+    let sql = `DELETE
+              FROM custom_set
+              WHERE set_id = ?`;
+    const [rows] = await conn.query(sql, [set_id]);
+
+    console.log("Redirecting to /customSets");
+    res.redirect("/customSets");
+  }
+); // delete custom set
 
 app.listen(3000, () => {
   console.log("Express server running");
 });
 
+// =========
 // Functions
+// =========
 
 // Used as a Middleware function to check if the user is signed in for certain routes
-function isAuthenticated(redirectPath) {
+function isNotAuthenticated(redirectPath) {
   return (req, res, next) => {
     if (!req.session.authenticated) {
       console.log("Redirecting to ", redirectPath);
@@ -282,7 +304,7 @@ function isAuthenticated(redirectPath) {
   };
 }
 
-function isNotAuthenticated(redirectPath) {
+function isAuthenticated(redirectPath) {
   return (req, res, next) => {
     if (req.session.authenticated) {
       console.log("Redirecting to ", redirectPath);
